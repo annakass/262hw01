c7eca5c45e5a3e38b3540cf7fbfda9842880b9d4
Anna <anna@Annas-MacBook-Pro.local>
2017-01-25 11:32:25 -0500

    update to right project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-public class sudoku {
-	
-	
-	
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/tile.java
+++
@@ -1,8 +0,0 @@
-
-public class tile { //microclass tile
-	int number;
-	int hcore; //x
-	int vcore; //y
-	int corectNum;
-	
-}
